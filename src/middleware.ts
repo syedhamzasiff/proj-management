0d429bee3772dcb6d8de31b4026f77ab76a07e06
syedhamzasiff <syedhamzasiff@gmail.com>
2024-11-25 11:59:43 +0500

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,22 @@ import { NextRequest, NextResponse } from 'next/server';
 import { decrypt } from './lib/auth';
 import { cookies } from 'next/headers';
 
-export const protectedRoutes =  ['/u', '/p', '/w'];
+export const protectedRoutes = ['/u', '/p', '/w'];
 const publicRoutes = ['/auth/login', '/auth/signup', '/'];
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoute = publicRoutes.includes(path);
+
+  const isProtectedRoute = protectedRoutes.some(route => path.startsWith(route));
+  const isPublicRoute = publicRoutes.some(route => path.startsWith(route));
 
   const cookie = (await cookies()).get('session')?.value;
-  const session = await decrypt(cookie);
+  const session = cookie ? await decrypt(cookie) : null;
+
+  console.log('Session:', session); 
 
   if (isProtectedRoute && !session?.userId) {
+    console.log('Redirecting to /auth/login...');
     return NextResponse.redirect(new URL('/auth/login', request.nextUrl));
   }
 
@@ -22,14 +26,9 @@ export async function middleware(request: NextRequest) {
     session?.userId &&
     !request.nextUrl.pathname.startsWith('/u/dashboard')
   ) {
+    console.log('Redirecting to /u/dashboard...');
     return NextResponse.redirect(new URL('/u/dashboard', request.nextUrl));
   }
 
   return NextResponse.next();
 }
-
-
-
-
-
-
